e8fe6b8fa34e6566a54c38a42b43b7d825be1ae9
uxname <uxname@gmail.com>
2025-03-17 02:42:56 +0300

    feat: Implement code analysis and refactoring with Gemini LLM

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import {CustomLLMService} from "./customLLMService.ts";
 import {createPrompt} from "./prompt-constructor.ts";
 import {z} from "zod";
+import * as fs from "node:fs";
+
+function loadTextFile(path: string): string {
+    const content = fs.readFileSync(path, 'utf-8');
+    // add string numbers
+    return content.split('\n').map((line, index) => `${index + 1}: ${line}`).join('\n');
+}
+
+function saveTextFile(path: string, content: string): void {
+    fs.writeFileSync(path, content);
+}
+
+const currentFile = import.meta.filename;
+const currentFileContent = loadTextFile(currentFile);
 
 async function main() {
     const llmService = new CustomLLMService(
@@ -8,24 +22,35 @@ async function main() {
         '123321ai'
     );
 
-    const template = '{a} + {b} = ?';
+    const template = 'Твоя задача проанализировать код и определить потенциальные проблемы и уязвимости. Также нужно предложить решения. Вот код:\n{code}';
+    const variables = {
+        code: currentFileContent
+    }
 
     const responseSchema = z.object({
-        result: z.number().describe("Результат выполнения задачи"),
+        potentialProblems: z.array(
+            z.object({
+                problem: z.string().describe("Потенциальная проблема"),
+                criticalLevel: z.enum(['LOW', 'MEDIUM', 'HIGH']).describe("Уровень критичности проблемы"),
+                stringStart: z.number().describe("Номер строки, с которой начинается проблема"),
+                stringEnd: z.number().describe("Номер строки, на которой заканчивается проблема"),
+                solution: z.string().describe("Решение")
+            })
+        ).describe("Массив потенциальных проблем и решений"),
+        potentialProblemCount: z.number().describe("Количество потенциальных проблем"),
+        // not empty string
+        refactoredCode: z.string().nonempty().describe("Код в котором исправлены все проблемы")
     });
 
     const prompt = createPrompt(
         template,
-        {
-            a: '1',
-            b: '2'
-        },
+        variables,
         responseSchema
     )
 
-    const result = await llmService.generateResponse({_prompt: prompt});
+    const result = await llmService.generateResponse({_prompt: prompt, provider: 'gemini'});
 
-    console.log('Result:', result);
+    // saveTextFile(`${import.meta.filename}.refactored.ts`, result.refactoredCode);
 }
 
 main();
