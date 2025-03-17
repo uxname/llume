34194c1a915b1c805f1225cf06fa4c57877696b3
uxname <uxname@gmail.com>
2025-03-17 13:25:07 +0300

    Refactor: Remove unnecessary response validation and cleaning functions

--- src/response-format-constructor.ts
+++ src/response-format-constructor.ts
@@ -6,13 +6,3 @@ export type ResponseSchema = object;
 export function generateSchema<T>(schema: z.ZodType<T>): ResponseSchema {
     return zodToJsonSchema(schema);
 }
-
-export function clearResponse(response: string): string {
-    // remove ```json
-    // remove ```
-    return response.replace(/```json/g, '').replace(/```/g, '');
-}
-
-export function validateResponse<T>(response: string, schema: z.ZodType<T>): T {
-    return schema.parse(JSON.parse(response));
-}
