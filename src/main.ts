948a6440497607eda839ce62cba2a8c326fff580
uxname <uxname@gmail.com>
2025-03-17 03:49:37 +0300

    feat: Add AgentRouter to route tasks to appropriate agents

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,18 @@ interface CalculatorResponse {
     errors: string[];
 }
 
+interface AgentWithAgent {
+    agent: string;
+    errors?: never;
+}
+
+interface AgentWithErrors {
+    agent?: never;
+    errors: string[];
+}
+
+type AgentRouterResponse = AgentWithAgent | AgentWithErrors;
+
 class Calculator extends MicroAgent<CalculatorResponse> {
     constructor(evaluation: string) {
         const config: MicroAgentConfig<CalculatorResponse> = {
@@ -28,18 +40,53 @@ class Calculator extends MicroAgent<CalculatorResponse> {
     }
 }
 
+class AgentRouter extends MicroAgent<AgentRouterResponse> {
+    constructor(
+        task: string,
+        agents: MicroAgent[]
+    ) {
+        const config: MicroAgentConfig<AgentRouterResponse> = {
+            name: 'Агент-роутер',
+            description: 'Определяет лучший агент для выполнения задачи',
+            template: 'Твоя задача выбрать наиболее подходящего агента для выполнения задачи. Вот задача: {task}, вот список доступных агентов: {agents}',
+            vars: {
+                task,
+                agents: JSON.stringify(agents.map(agent => agent.toString()))
+            },
+            responseSchema: z.union([
+                z.object({
+                    agent: z.string().describe('Имя выбранного агента'),
+                    errors: z.undefined(), // Убедитесь, что errors не определено
+                }),
+                z.object({
+                    agent: z.undefined(), // Убедитесь, что agent не определено
+                    errors: z.array(z.string()).describe('Список ошибок, если есть, например если подходящего агента нет'),
+                }),
+            ])
+        }
+
+        super(config);
+    }
+}
+
 async function main() {
     const llmService = new CustomLLMService(
         'https://ai0.uxna.me/',
         '123321ai'
     );
 
-    const calculator = new Calculator('2 + 2 * 2');
-    console.log('Calculator agent info:', calculator.toString())
+    // const query = 'Какая погода в Минске обычно летом?';
+    const query = 'сколько будет 9 в квадрате?';
 
-    const result = await calculator.execute(llmService, 'gemini');
+    const calculator = new Calculator('сколько будет 9 в квадрате?');
 
-    console.log(result.value);
+    const agentRouter = new AgentRouter(query, [calculator]);
+
+    const calculatorResult = await calculator.execute(llmService, 'gemini');
+    const agentRouterResult = await agentRouter.execute(llmService, 'gemini');
+
+    console.log('calculatorResult', calculatorResult.value);
+    console.log('agentRouterResult', agentRouterResult.agent);
 }
 
 main();
