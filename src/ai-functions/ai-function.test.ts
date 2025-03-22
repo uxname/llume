355ef8e093a80545835ce3c3b78d6ea6fc8f2d28
uxname <uxname@gmail.com>
2025-03-22 22:04:16 +0300

    feat: Implement basic AiFunction test with calculator example and AI0 engine

--- src/ai-functions/ai-function.test.ts
+++ src/ai-functions/ai-function.test.ts
@@ -3,13 +3,14 @@ import {Prompt} from "../prompt/prompt.ts";
 import {AiFunction} from "./ai-function.ts";
 import {z} from "zod";
 import {AiExecutionEngineBase} from "../ai-execution-engine/ai-execution-engine-base.ts";
+import {Ai0} from "../ai-execution-engine/engines/ai0/ai0.ts";
 
 describe('AiFunction', () => {
     test('renders basic template with params', () => {
         const prompt = new Prompt('Calculate {value1} + {value2} = ?');
         const schema = z.object({
-            value: z.number().nullable().describe('Результат выражения'),
-            errors: z.array(z.string()).nullable().describe('Список ошибок, если есть'),
+            value: z.number().nullable().describe('Expression result'),
+            errors: z.array(z.string()).nullable().describe('List of errors if any'),
         });
 
         type CalculatorResponse = typeof schema;
@@ -17,17 +18,22 @@ describe('AiFunction', () => {
         class TestAiFunction extends AiFunction<CalculatorResponse> {
             constructor(aiExecutionEngine?: AiExecutionEngineBase) {
                 super({
-                    name: 'Калькулятор',
-                    description: 'Вычисляет математические выражения',
-                    prompt: new Prompt('Ты точный калькулятор, посчитай и выдай результат следующего выражения: {evaluation}'),
+                    name: 'Calculator',
+                    description: 'Calculates mathematical expressions',
+                    prompt: new Prompt('You are a true calculator, calculate and return the result of the following expression: {evaluation}'),
                     responseSchema: schema,
                     aiExecutionEngine
                 });
             }
         }
 
-        const aiFunction = new TestAiFunction();
+        const engine = new Ai0(process.env.AI0_URL!, process.env.AI0_API_KEY!);
+        const aiFunction = new TestAiFunction(engine);
         const renderedPrompt = aiFunction.render({evaluation: '2 + 2'});
-        console.log(renderedPrompt);
+        expect(renderedPrompt).toEqual(`You are a true calculator, calculate and return the result of the following expression: 2 + 2
+Answer format json should according to the following JSON schema:
+{"type":"object","properties":{"value":{"type":["number","null"],"description":"Expression result"},"errors":{"anyOf":[{"type":"array","items":{"type":"string"}},{"type":"null"}],"description":"List of errors if any"}},"required":["value","errors"],"additionalProperties":false,"$schema":"http://json-schema.org/draft-07/schema#"}
+Do not send any other data. Do not send markdown.`);
+
     });
 });
