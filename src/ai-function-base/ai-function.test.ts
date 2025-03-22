ce119a42444e945a60c37e7fd41ebef77200ff5e
uxname <uxname@gmail.com>
2025-03-23 02:43:21 +0300

    feat(ai-function): Add `beforeAll` hook for setup & generate function info test.

--- src/ai-function-base/ai-function.test.ts
+++ src/ai-function-base/ai-function.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { beforeAll, describe, expect, test } from "vitest";
 import { Prompt } from "../prompt/prompt.ts";
 import { AiFunction } from "./ai-function.ts";
 import { z } from "zod";
@@ -7,33 +7,35 @@ import { Ai0 } from "../ai-execution-engine/engines/ai0/ai0.ts";
 import { Container } from "../container.ts";
 
 describe("AiFunction", () => {
-  test("renders basic template with params", async () => {
-    const schema = z.object({
-      value: z.number().nullable().describe("Expression result"),
-      errors: z.array(z.string()).nullable().describe("List of errors if any"),
-    });
+  const schema = z.object({
+    value: z.number().nullable().describe("Expression result"),
+    errors: z.array(z.string()).nullable().describe("List of errors if any"),
+  });
 
-    type CalculatorResponse = typeof schema;
+  type CalculatorResponse = typeof schema;
 
-    class TestAiFunction extends AiFunction<CalculatorResponse> {
-      constructor(aiExecutionEngine?: AiExecutionEngineBase) {
-        super({
-          name: "Calculator",
-          description: "Calculates mathematical expressions",
-          prompt: new Prompt(
-            "You are a true calculator, calculate and return the result of the following expression: {evaluation}",
-          ),
-          responseSchema: schema,
-          aiExecutionEngine,
-        });
-      }
+  class TestAiFunction extends AiFunction<CalculatorResponse> {
+    constructor() {
+      super({
+        name: "Calculator",
+        description: "Calculates mathematical expressions",
+        prompt: new Prompt(
+          "You are a true calculator, calculate and return the result of the following expression: {evaluation}",
+        ),
+        responseSchema: schema,
+      });
     }
+  }
 
-    const engine = new Ai0(process.env.AI0_URL!, process.env.AI0_API_KEY!);
-    const aiFunction = new TestAiFunction(engine);
-    const container = new Container(engine);
+  const engine = new Ai0(process.env.AI0_URL!, process.env.AI0_API_KEY!);
+  const aiFunction = new TestAiFunction(engine);
+  const container = new Container(engine);
+
+  beforeAll(() => {
     container.addAiFunction(aiFunction);
+  });
 
+  test("renders basic template with params", async () => {
     const evaluation = "2 + 2";
     const renderedPrompt = aiFunction.render({ evaluation });
     expect(renderedPrompt)
@@ -47,5 +49,24 @@ Do not send unknown other data. Do not send markdown.`);
     expect(result.value).toBe(4);
     expect(result._error).toBeUndefined();
     expect(result._raw).toBeDefined();
+  });
+
+  test("should generate function info", () => {
+    class TestAiFunction extends AiFunction<CalculatorResponse> {
+      constructor() {
+        super({
+          name: "Calculator",
+          description: "Calculates mathematical expressions",
+          prompt: new Prompt(
+            "You are a true calculator, calculate and return the result of the following expression: {evaluation}",
+          ),
+          responseSchema: schema,
+        });
+      }
+    }
+    const aiFunction = new TestAiFunction();
+    const info = aiFunction.toJson();
+    expect(info.name).toBe("Calculator");
+    expect(info.description).toBe("Calculates mathematical expressions");
   });
 });
