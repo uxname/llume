f4502bc78ee6b844c7d8e9eb31623f4a1b1c4534
uxname <uxname@gmail.com>
2025-03-24 00:11:27 +0300

    feat: Implement line counter AI function and tests

---
+++ src/ai-functions/line-counter/line-counter.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "vitest";
+import { Ai0 } from "../../ai-execution-engine/engines/ai0/ai0.ts";
+import { LineCounter } from "./line-counter.ts";
+import { Container } from "../../container.ts";
+
+describe("Line counter", () => {
+  const engine = new Ai0(process.env.AI0_URL!, process.env.AI0_API_KEY!);
+  const container = new Container(engine);
+
+  const lineCounter = new LineCounter();
+  container.registerAiFunction(lineCounter);
+
+  test("should count lines", async () => {
+    const result = await lineCounter.execute({
+      text: "Just a test\n".repeat(10),
+    });
+
+    expect(result.count).toBe(10);
+  });
+});
---
+++ src/ai-functions/line-counter/line-counter.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { Prompt } from "../../prompt/prompt.ts";
+import { AiFunction } from "../../ai-function-base/ai-function.ts";
+
+const schema = z.object({
+  count: z.number().nullable().describe("Count of lines"),
+});
+
+export type LineCounterResponse = typeof schema;
+
+export class LineCounter extends AiFunction<LineCounterResponse> {
+  constructor() {
+    super({
+      name: "Line Counter",
+      description: "Counts the number of lines in a text",
+      prompt: new Prompt(
+        "You are a true line counter, count and return the number of lines in the following text: {text}",
+      ),
+      responseSchema: schema,
+    });
+  }
+}
