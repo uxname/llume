db110440d685d96695b3ebaf75418d509c38dace
uxname <uxname@gmail.com>
2025-03-23 19:48:15 +0300

    feat(file-reader): Implement file reader tool

---
+++ src/tool-base/tool-base.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export interface ToolMetadata<
+  INPUT extends z.ZodType = z.ZodType,
+  OUTPUT extends z.ZodType = z.ZodType,
+> {
+  name: string;
+  description: string;
+  inputSchema: INPUT;
+  outputSchema: OUTPUT;
+  examples?: string[];
+}
+
+export abstract class ToolBase<
+  TInput extends z.infer<z.ZodType>,
+  TOutput extends z.infer<z.ZodType>,
+> {
+  abstract getMetadata(): ToolMetadata;
+
+  abstract execute(params: TInput): Promise<TOutput>;
+
+  toString(): string {
+    return JSON.stringify(this.getMetadata());
+  }
+}
---
+++ src/tools/file-reader/file-reader.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "vitest";
+import { FileReader } from "./file-reader.ts";
+
+describe("File reader", () => {
+  test("should read file", async () => {
+    const fileReader = new FileReader();
+    const filePath = "test.txt";
+    const fileContent = await fileReader.execute({ path: filePath });
+
+    expect(fileContent).toEqual({
+      content: `Content of ${filePath}\n`.repeat(10),
+      name: filePath,
+    });
+
+    expect(fileReader.toString()).toEqual(
+      '{"name":"File Reader","description":"Reads the content of a file","inputSchema":{"_def":{"unknownKeys":"strip","catchall":{"_def":{"typeName":"ZodNever"},"~standard":{"version":1,"vendor":"zod"}},"typeName":"ZodObject"},"~standard":{"version":1,"vendor":"zod"},"_cached":null},"outputSchema":{"_def":{"unknownKeys":"strip","catchall":{"_def":{"typeName":"ZodNever"},"~standard":{"version":1,"vendor":"zod"}},"typeName":"ZodObject"},"~standard":{"version":1,"vendor":"zod"},"_cached":null}}',
+    );
+  });
+});
