9b6f36c2a1ecc79bfa00eb472293e7e878ab78d8
uxname <uxname@gmail.com>
2025-03-24 02:02:42 +0300

    feat: Move tool test to fake-file-reader.test.ts for better organization.

--- src/container.test.ts
+++ src/container.test.ts
@@ -2,8 +2,6 @@ import { describe, expect, test } from "vitest";
 import { Ai0 } from "./ai-execution-engine/engines/ai0/ai0.ts";
 import { Container } from "./container.ts";
 import { Calculator } from "./ai-functions/calculator/calculator.ts";
-import { FakeFileReader } from "./tools/fake-file-reader/fake-file-reader.ts";
-import { LineCounter } from "./ai-functions/line-counter/line-counter.ts";
 
 describe("Container", () => {
   const engine = new Ai0(process.env.AI0_URL!, process.env.AI0_API_KEY!);
@@ -20,22 +18,5 @@ describe("Container", () => {
     });
 
     expect(result.value).toBe(5);
-  });
-
-  test("should call tool", async () => {
-    const container = new Container(engine);
-
-    const lineCounter = new LineCounter();
-    container.registerAiFunction(lineCounter);
-
-    const fakeFileReaderTool = new FakeFileReader();
-    container.addTool(fakeFileReaderTool);
-
-    const result = await lineCounter.execute({
-      filePath: "test.txt",
-    });
-
-    expect(result.count).toBeGreaterThanOrEqual(0);
-    expect(result.count).toBeLessThanOrEqual(30);
   });
 });
--- src/tools/fake-file-reader/fake-file-reader.test.ts
+++ src/tools/fake-file-reader/fake-file-reader.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, test } from "vitest";
 import { FakeFileReader } from "./fake-file-reader.ts";
+import { Container } from "../../container.ts";
+import { LineCounter } from "../../ai-functions/line-counter/line-counter.ts";
+import { Ai0 } from "../../ai-execution-engine/engines/ai0/ai0.ts";
 
 describe("Fake File reader", () => {
   test("should read file", async () => {
@@ -8,5 +11,23 @@ describe("Fake File reader", () => {
     const fileContent = await fileReader.execute({ path: filePath });
 
     expect(fileContent.content).contains("Content of test.txt");
+  });
+
+  test("should call tool", async () => {
+    const engine = new Ai0(process.env.AI0_URL!, process.env.AI0_API_KEY!);
+    const container = new Container(engine);
+
+    const lineCounter = new LineCounter();
+    container.registerAiFunction(lineCounter);
+
+    const fakeFileReaderTool = new FakeFileReader();
+    container.addTool(fakeFileReaderTool);
+
+    const result = await lineCounter.execute({
+      filePath: "test.txt",
+    });
+
+    expect(result.count).toBeGreaterThanOrEqual(0);
+    expect(result.count).toBeLessThanOrEqual(30);
   });
 });
